bcc29d975ff604ea286eb7757cb844eb5f39f1d9
Kris-The-Fish <witehiram4172@masseyhigh.school.nz>
2026-02-23 12:14:30 +1300

    broken ):

--- B_01_RPS_game_v1.py
+++ B_01_RPS_game_v1.py
@@ -1,3 +1,51 @@
+# Check that users have entered a valid
+# option based on a list
+def string_checker(question, valid_ans=('yes', 'no')):
+
+    error = f"Please enter a valid option from the following list: {valid_ans}"
+
+    while True:
+
+        # Get user response and make sure it's lowercase
+        user_response = input(question).lower()
+
+        for item in valid_ans:
+            # check if the user response in a word in the list
+            if item == user_response:
+                return item
+
+            # check if the user response is the same as
+            # the first letter of an item in the list
+            elif user_response == item[0]:
+                return item
+
+        # print error if user does not enter something that is valid
+        print(error)
+        print()
+
+
+def instructions():
+    """prints instructions"""
+
+    print("""
+*** Instructions ****
+
+ To begin, choose the number of rounds (or press <enter> for
+  play infinite mode).
+
+ Then play against the computer. You need to choose R (rock),
+  P (paper), or S (scissors).
+
+ The rules are as follows:
+ - Paper beats Rock
+ - Rock beats Scissors
+ - Scissors beats Paper
+
+ Press <xxx> to end the game at anytime>
+
+ Good Luck !!!
+    """)
+
 def int_check(question):
 
     while True:
@@ -21,47 +69,58 @@ def int_check(question):
             print(error)
 
 
-
-
 # Main Routine Starts here
 
 # Inialise game variables
 mode = "regular"
 rounds_played = 0
 
+rps_list = ["Rock", "Paper", "Scissors", "XXX"]
+
 
 print("💎📃✂️Rock / Paper / Scissors Game !✂️📃💎")
 print()
 
-# Instructions
+# ask the user if they want instructions (check they say yes / no)
+want_instructions = string_checker("do you want to see the instructions? ")
+
+# Display the instructions if the user wants to see them...
+if want_instructions == "yes":
+    instructions()
+
 
 # Ask user for number of rounds / infinite mode
 num_rounds = int_check("How many rounds would you like? Push <enter> for infinite mode: ")
 
 if num_rounds == "infinite":
     mode = "infinite"
-    print("you chose infinite")
     num_rounds = 5
 
 # Game loop starts here
 while rounds_played < num_rounds:
-    user_choice = input("Choose: ")
+
+
+    # Rounds headings
+    if mode == "infinite":
+        rounds_heading = f"\n 📀📀📀 Round {rounds_played + 1} (Infinite Mode) 📀📀📀"
+    else:
+        rounds_heading = f"\n 💿💿💿 Round {rounds_played + 1} of {num_rounds} 💿💿💿"
+
+    print(rounds_heading)
+    print()
+
+    user_choice = string_checker("Choose: ", rps_list)
+    print("You chose", user_choice)
 
     if user_choice == "xxx":
         break
 
     rounds_played += 1
-    print("rounds played: ", rounds_played)
 
      # if users are infinite mode, increase number of rounds !
     if mode == "infinite":
         num_rounds += 1
 
-    print("num rounds: ", num_rounds)
-
-while rounds_played < num_rounds:
-    input("Choose: ")
-    rounds_played += 1
 
 # Game loop ends here
 
